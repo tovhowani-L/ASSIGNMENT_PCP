3ac4db69004b1da54b8556399f6e7e074165d42f
ttovh <ttovh@LENOVO>
2025-03-14 15:37:26 +0200

    Add project files.

---
+++ WinFormsApp4/Form1.Designer.vb
@@ -0,0 +1,353 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        RADGAMINGLAPTOP = New RadioButton()
+        RADBUSINESSLAPTOP = New RadioButton()
+        RADSTUDENTLAPTOP = New RadioButton()
+        CHKWIRELESSMOUSE = New CheckBox()
+        CHKMECHANICALKEYBOARD = New CheckBox()
+        CHKLAPTOPBAGG = New CheckBox()
+        LSTDISPLAY = New ListBox()
+        TXTAMOUNTB4PROMO = New TextBox()
+        TXTPROMODISCOUNT = New TextBox()
+        TXTVAT = New TextBox()
+        TXTTOTAL = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        calculate = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        TXTCONTACT = New TextBox()
+        TXTSURNAME = New TextBox()
+        TXTNAME = New TextBox()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(27, 308)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(60, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "LAPTOP"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(208, 308)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(101, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "ACCESSORIES"
+        ' 
+        ' RADGAMINGLAPTOP
+        ' 
+        RADGAMINGLAPTOP.AutoSize = True
+        RADGAMINGLAPTOP.Location = New Point(20, 354)
+        RADGAMINGLAPTOP.Name = "RADGAMINGLAPTOP"
+        RADGAMINGLAPTOP.Size = New Size(143, 24)
+        RADGAMINGLAPTOP.TabIndex = 2
+        RADGAMINGLAPTOP.Text = "GAMING LAPTOP"
+        RADGAMINGLAPTOP.UseVisualStyleBackColor = True
+        ' 
+        ' RADBUSINESSLAPTOP
+        ' 
+        RADBUSINESSLAPTOP.AutoSize = True
+        RADBUSINESSLAPTOP.Location = New Point(20, 395)
+        RADBUSINESSLAPTOP.Name = "RADBUSINESSLAPTOP"
+        RADBUSINESSLAPTOP.Size = New Size(151, 24)
+        RADBUSINESSLAPTOP.TabIndex = 3
+        RADBUSINESSLAPTOP.Text = "BUSINESS LAPTOP"
+        RADBUSINESSLAPTOP.UseVisualStyleBackColor = True
+        ' 
+        ' RADSTUDENTLAPTOP
+        ' 
+        RADSTUDENTLAPTOP.AutoSize = True
+        RADSTUDENTLAPTOP.Location = New Point(19, 436)
+        RADSTUDENTLAPTOP.Name = "RADSTUDENTLAPTOP"
+        RADSTUDENTLAPTOP.Size = New Size(149, 24)
+        RADSTUDENTLAPTOP.TabIndex = 4
+        RADSTUDENTLAPTOP.Text = "STUDENT LAPTOP"
+        RADSTUDENTLAPTOP.UseVisualStyleBackColor = True
+        ' 
+        ' CHKWIRELESSMOUSE
+        ' 
+        CHKWIRELESSMOUSE.AutoSize = True
+        CHKWIRELESSMOUSE.Location = New Point(208, 355)
+        CHKWIRELESSMOUSE.Name = "CHKWIRELESSMOUSE"
+        CHKWIRELESSMOUSE.Size = New Size(151, 24)
+        CHKWIRELESSMOUSE.TabIndex = 8
+        CHKWIRELESSMOUSE.Text = "WIRELESS MOUSE"
+        CHKWIRELESSMOUSE.UseVisualStyleBackColor = True
+        ' 
+        ' CHKMECHANICALKEYBOARD
+        ' 
+        CHKMECHANICALKEYBOARD.AutoSize = True
+        CHKMECHANICALKEYBOARD.Location = New Point(209, 395)
+        CHKMECHANICALKEYBOARD.Name = "CHKMECHANICALKEYBOARD"
+        CHKMECHANICALKEYBOARD.Size = New Size(202, 24)
+        CHKMECHANICALKEYBOARD.TabIndex = 9
+        CHKMECHANICALKEYBOARD.Text = "MECHANICAL KEYBOARD"
+        CHKMECHANICALKEYBOARD.UseVisualStyleBackColor = True
+        ' 
+        ' CHKLAPTOPBAGG
+        ' 
+        CHKLAPTOPBAGG.AutoSize = True
+        CHKLAPTOPBAGG.Location = New Point(209, 437)
+        CHKLAPTOPBAGG.Name = "CHKLAPTOPBAGG"
+        CHKLAPTOPBAGG.Size = New Size(115, 24)
+        CHKLAPTOPBAGG.TabIndex = 10
+        CHKLAPTOPBAGG.Text = "LAPTOP BAG"
+        CHKLAPTOPBAGG.UseVisualStyleBackColor = True
+        ' 
+        ' LSTDISPLAY
+        ' 
+        LSTDISPLAY.FormattingEnabled = True
+        LSTDISPLAY.Location = New Point(356, 39)
+        LSTDISPLAY.Name = "LSTDISPLAY"
+        LSTDISPLAY.Size = New Size(443, 224)
+        LSTDISPLAY.TabIndex = 11
+        ' 
+        ' TXTAMOUNTB4PROMO
+        ' 
+        TXTAMOUNTB4PROMO.Location = New Point(602, 289)
+        TXTAMOUNTB4PROMO.Name = "TXTAMOUNTB4PROMO"
+        TXTAMOUNTB4PROMO.Size = New Size(125, 27)
+        TXTAMOUNTB4PROMO.TabIndex = 12
+        ' 
+        ' TXTPROMODISCOUNT
+        ' 
+        TXTPROMODISCOUNT.Location = New Point(602, 335)
+        TXTPROMODISCOUNT.Name = "TXTPROMODISCOUNT"
+        TXTPROMODISCOUNT.Size = New Size(125, 27)
+        TXTPROMODISCOUNT.TabIndex = 13
+        ' 
+        ' TXTVAT
+        ' 
+        TXTVAT.Location = New Point(602, 375)
+        TXTVAT.Name = "TXTVAT"
+        TXTVAT.Size = New Size(125, 27)
+        TXTVAT.TabIndex = 14
+        ' 
+        ' TXTTOTAL
+        ' 
+        TXTTOTAL.Location = New Point(602, 422)
+        TXTTOTAL.Name = "TXTTOTAL"
+        TXTTOTAL.Size = New Size(125, 27)
+        TXTTOTAL.TabIndex = 15
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(422, 308)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(149, 20)
+        Label3.TabIndex = 16
+        Label3.Text = "AMOUNT B4 PROMO"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(425, 422)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(50, 20)
+        Label4.TabIndex = 17
+        Label4.Text = "TOTAL"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(422, 391)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(34, 20)
+        Label5.TabIndex = 18
+        Label5.Text = "VAT"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(422, 348)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(137, 20)
+        Label6.TabIndex = 19
+        Label6.Text = "PROMO DISCOUNT"
+        ' 
+        ' calculate
+        ' 
+        calculate.Location = New Point(24, 497)
+        calculate.Name = "calculate"
+        calculate.Size = New Size(94, 29)
+        calculate.TabIndex = 20
+        calculate.Text = "CALCULATE"
+        calculate.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(447, 494)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 21
+        Button2.Text = "EXIT"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(322, 497)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 22
+        Button3.Text = "CAPTURE"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(169, 497)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 23
+        Button4.Text = "CLEAR"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' TXTCONTACT
+        ' 
+        TXTCONTACT.Location = New Point(194, 181)
+        TXTCONTACT.Name = "TXTCONTACT"
+        TXTCONTACT.Size = New Size(125, 27)
+        TXTCONTACT.TabIndex = 24
+        ' 
+        ' TXTSURNAME
+        ' 
+        TXTSURNAME.Location = New Point(194, 137)
+        TXTSURNAME.Name = "TXTSURNAME"
+        TXTSURNAME.Size = New Size(125, 27)
+        TXTSURNAME.TabIndex = 25
+        ' 
+        ' TXTNAME
+        ' 
+        TXTNAME.Location = New Point(194, 97)
+        TXTNAME.Name = "TXTNAME"
+        TXTNAME.Size = New Size(125, 27)
+        TXTNAME.TabIndex = 26
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(36, 104)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(51, 20)
+        Label7.TabIndex = 27
+        Label7.Text = "NAME"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(36, 188)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(74, 20)
+        Label8.TabIndex = 28
+        Label8.Text = "CONTACT"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(36, 144)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(78, 20)
+        Label9.TabIndex = 29
+        Label9.Text = "SURNAME"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(825, 605)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(TXTNAME)
+        Controls.Add(TXTSURNAME)
+        Controls.Add(TXTCONTACT)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(calculate)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(TXTTOTAL)
+        Controls.Add(TXTVAT)
+        Controls.Add(TXTPROMODISCOUNT)
+        Controls.Add(TXTAMOUNTB4PROMO)
+        Controls.Add(LSTDISPLAY)
+        Controls.Add(CHKLAPTOPBAGG)
+        Controls.Add(CHKMECHANICALKEYBOARD)
+        Controls.Add(CHKWIRELESSMOUSE)
+        Controls.Add(RADSTUDENTLAPTOP)
+        Controls.Add(RADBUSINESSLAPTOP)
+        Controls.Add(RADGAMINGLAPTOP)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RADGAMINGLAPTOP As RadioButton
+    Friend WithEvents RADBUSINESSLAPTOP As RadioButton
+    Friend WithEvents RADSTUDENTLAPTOP As RadioButton
+    Friend WithEvents CHKWIRELESSMOUSE As CheckBox
+    Friend WithEvents CHKMECHANICALKEYBOARD As CheckBox
+    Friend WithEvents CHKLAPTOPBAGG As CheckBox
+    Friend WithEvents LSTDISPLAY As ListBox
+    Friend WithEvents TXTAMOUNTB4PROMO As TextBox
+    Friend WithEvents TXTPROMODISCOUNT As TextBox
+    Friend WithEvents TXTVAT As TextBox
+    Friend WithEvents TXTTOTAL As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents calculate As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TXTCONTACT As TextBox
+    Friend WithEvents TXTSURNAME As TextBox
+    Friend WithEvents TXTNAME As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+
+End Class
---
+++ WinFormsApp4/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.DirectoryServices
+
+Public Class Form1
+    Private Sub CHKLAPTOPBAGG_CheckedChanged(sender As Object, e As EventArgs) Handles CHKLAPTOPBAGG.CheckedChanged
+
+    End Sub
+    Private Sub LSTDISPLAY_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LSTDISPLAY.SelectedIndexChanged
+
+    End Sub
+
+
+
+    Private Sub calculate_Click(sender As Object, e As EventArgs) Handles calculate.Click
+        Dim STRNAME, STRSURNAME As String
+        Dim INTCONTACT As Integer
+        Dim DECLAPTOP, LAPTOPDECPRICE, DECWIRELESSMOUSE, DECMECHANICALKEYBOARD, DECSTUDENTLAPTOP, DECLAPTOPBAG As Decimal
+        Dim DECAMOUNTB4PROMO, DECPROMODISCOUNT, DECVAT, DECTOTAL As Decimal
+        STRNAME = CStr(TXTNAME.Text)
+        STRSURNAME = CStr(TXTSURNAME.Text)
+        INTCONTACT = CInt(TXTCONTACT.Text)
+        LSTDISPLAY.Items.Add("QUOTATION FOR:" & vbTab & TXTNAME.Text & vbTab & TXTSURNAME.Text)
+        LSTDISPLAY.Items.Add("CUSTOMER CONTACT:" & vbTab & TXTCONTACT.Text)
+        LSTDISPLAY.Items.Add("........................................................")
+        If RADGAMINGLAPTOP.Checked Then
+            LAPTOPDECPRICE = 10000
+            LSTDISPLAY.Items.Add("LAPTOP NAME: GAMING LAPTOP" & vbTab & LAPTOPDECPRICE)
+        End If
+        If RADBUSINESSLAPTOP.Checked Then
+            LAPTOPDECPRICE = 7000
+            LSTDISPLAY.Items.Add("LAPTOP NAME: BUSINESS LAPTOP" & vbTab & LAPTOPDECPRICE)
+        End If
+        If RADSTUDENTLAPTOP.Checked Then
+            LAPTOPDECPRICE = 4500
+            LSTDISPLAY.Items.Add("LAPTOP NAME: STUDENT LAPTOP" & vbTab & LAPTOPDECPRICE)
+        End If
+        If CHKWIRELESSMOUSE.Checked = True Then
+            DECWIRELESSMOUSE = 100
+            LSTDISPLAY.Items.Add("WIRELESSMOUSE:" & vbTab & DECWIRELESSMOUSE)
+        End If
+        If CHKMECHANICALKEYBOARD.Checked = True Then
+            DECMECHANICALKEYBOARD = 150
+            LSTDISPLAY.Items.Add("MECHANICALKEYBOARD:" & vbTab & DECMECHANICALKEYBOARD)
+        End If
+        If CHKLAPTOPBAGG.Checked = True Then
+            DECLAPTOPBAG = 250
+            LSTDISPLAY.Items.Add("LAPTOPBAG:" & vbTab & DECLAPTOPBAG)
+        End If
+        DECAMOUNTB4PROMO = (LAPTOPDECPRICE + DECMECHANICALKEYBOARD + DECWIRELESSMOUSE + DECLAPTOPBAG)
+        DECPROMODISCOUNT = (DECAMOUNTB4PROMO) -
+        TXTPROMODISCOUNT.Text = DECPROMODISCOUNT.ToString()
+
+
+
+
+
+        DECVAT = (DECAMOUNTB4PROMO) * (0.15)
+        DECTOTAL = (DECPROMODISCOUNT) + (DECVAT)
+        TXTAMOUNTB4PROMO.Text = DECAMOUNTB4PROMO.ToString("C")
+        TXTPROMODISCOUNT.Text = DECLAPTOP.ToString("C")
+        TXTVAT.Text = DECVAT.ToString("C")
+        TXTTOTAL.Text = DECTOTAL.ToString("C")
+    End Sub
+
+
+End Class
